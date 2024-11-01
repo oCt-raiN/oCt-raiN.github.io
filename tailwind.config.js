7646fbb68182a4579296fe0e5d961e12328f42df
Aathi Manikandan <aathi257800@gmail.com>
2024-11-01 22:54:47 +0000

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
+import CONFIG from './gitprofile.config';
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  prefix: 're',
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/*.{js,ts,jsx,tsx,mdx}'
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [require('daisyui')],
+  daisyui: {
+    logs: false,
+    themes: [
+      ...CONFIG.themeConfig.themes,
+      { procyon: CONFIG.themeConfig.customTheme },
+    ],
+  },
+};
